491a5a6b76f4a32553bc6d0bcd941f5d7721b4b4
abbasi <muhammad.ahsan_abbasi@mail.schwarz>
2022-12-07 11:56:03 +0100

    challenge-2

--- src/Controller/Attendee/ReadController.php
+++ src/Controller/Attendee/ReadController.php
@@ -7,13 +7,16 @@ namespace App\Controller\Attendee;
 use App\Entity\Attendee;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/attendees/{identifier}', name: 'read_attendee', methods: ['GET'])]
 final class ReadController
 {
-    public function __invoke(Attendee $attendee): Response
+    public function __invoke(Attendee $attendee, SerializerInterface $serializer): Response
     {
-        return new Response(json_encode($attendee->toArray()), Response::HTTP_OK, [
+        $serializedAttendee = $serializer->serialize($attendee, 'json');
+
+        return new Response($serializedAttendee, Response::HTTP_OK, [
             'Content-Type' => 'application/json',
         ]);
     }
